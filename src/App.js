ed57dae40e7b42ea9d8b23523599ffe900e620ae
sawanicodelabs <Sawan@icodelabs.co>
2021-05-14 18:11:24 +0530

    some fixup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
